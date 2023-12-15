f82f905fae4bfd0bfc755398d8d19c85ac286f37
Abdullah Al Noman <111496179+Abdullahal2010@users.noreply.github.com>
2023-12-15 18:39:30 +0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+import Board from "./components/TIC_TAK_TOE/index";
+
+const App = () => {
+  return (
+    <>
+      <Board />
+    </>
+  );
+};
+
+export default App;
